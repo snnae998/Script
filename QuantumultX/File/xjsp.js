bb2afa73212a985884c775ba0e60b997181a1e54
snnae998 <58739325+snnae998@users.noreply.github.com>
2019-12-12 21:01:46 +0800

    Update xjsp.js

--- QuantumultX/File/xjsp.js
+++ QuantumultX/File/xjsp.js
@@ -1,3 +1,7 @@
+/**
+ * @supported 231E62EBF
+ */
+
 /*
 Banana video unlock vip
 app download link : http://tinyurl.com/y57j6hjg
@@ -47,8 +51,4 @@ if (url.indexOf(path3) != -1){
 }
 $done({body});
 
-/**
- * @supported 231E62EBF
- */
 
-//（Made by Meeta）
